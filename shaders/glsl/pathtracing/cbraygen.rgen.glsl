1cea603eaa34f34ba31d70c58f72204b260a76e1
realDqt <2247334208@qq.com>
2025-12-10 14:35:59 +0800

    Add path tracing function

--- shaders/glsl/pathtracing/cbraygen.rgen.glsl
+++ shaders/glsl/pathtracing/cbraygen.rgen.glsl
@@ -14,6 +14,8 @@ layout(binding = 2, set = 0) uniform CameraProperties{
 
 layout(binding = 3, set = 0) buffer GeometryNodes {GeometryNode nodes[];}geometryNodes;
 
+layout(binding = 4, set = 0) buffer Materials {Material mats[];} materials;
+
 layout(binding = 5, set = 0) buffer LightData{
     mat4 objectToWorld;
     int lightGeometryIndex;
@@ -21,78 +23,14 @@ layout(binding = 5, set = 0) buffer LightData{
 
 layout(location = 0) rayPayloadEXT RayPayload hitValue;
 
-// Tiny Encryption Algorithm
-// By Fahad Zafar, Marc Olano and Aaron Curtis, see https://www.highperformancegraphics.org/previous/www_2010/media/GPUAlgorithms/HPG2010_GPUAlgorithms_Zafar.pdf
-uint tea(uint val0, uint val1)
-{
-    uint sum = 0;
-    uint v0 = val0;
-    uint v1 = val1;
-    for (uint n = 0; n < 16; n++)
-    {
-        sum += 0x9E3779B9;
-        v0 += ((v1 << 4) + 0xA341316C) ^ (v1 + sum) ^ ((v1 >> 5) + 0xC8013EA4);
-        v1 += ((v0 << 4) + 0xAD90777D) ^ (v0 + sum) ^ ((v0 >> 5) + 0x7E95761E);
-    }
-    return v0;
-}
+struct LightSampleRes{
+    vec3 pos;
+    vec3 normal;
+    vec3 emit;
+};
 
-// Linear congruential generator based on the previous RNG state
-// See https://en.wikipedia.org/wiki/Linear_congruential_generator
-uint lcg(inout uint previous)
-{
-    const uint multiplier = 1664525u;
-    const uint increment = 1013904223u;
-    previous   = (multiplier * previous + increment);
-    return previous & 0x00FFFFFFu;
-}
 
-// Generate a random float in [0, 1) given the previous RNG state
-float rnd(inout uint previous)
-{
-    return (float(lcg(previous)) / float(0x01000000));
-}
-
-// 计算三角形面积
-// 原理：三角形面积等于两边叉积模长的一半
-float cacTriangleArea(vec3 worldPositions[3])
-{
-    vec3 edge1 = vec3(worldPositions[1] - worldPositions[0]);
-    vec3 edge2 = vec3(worldPositions[2] - worldPositions[0]);
-
-    // cross(edge1, edge2) 得到法向量（长度为平行四边形面积）
-    // length 取模长，然后除以 2 得到三角形面积
-    float area = 0.5 * length(cross(edge1, edge2));
-
-    return area;
-}
-
-// 均匀采样三角形表面的点
-// 注意：必须传入 seed 用于生成随机数，且必须使用 sqrt 校正分布
-vec3 uniformSampleTriangle(vec3 worldPositions[3], inout uint seed)
-{
-    // 1. 获取两个 [0, 1) 的随机数
-    // 假设你有之前定义的 rnd() 函数
-    float r1 = rnd(seed);
-    float r2 = rnd(seed);
-
-    // 2. 计算重心坐标 (Barycentric Coordinates)
-    // 关键：必须对 r1 开根号，否则采样点会聚集在 worldPositions[0] 附近
-    float sqrtR1 = sqrt(r1);
-
-    float u = 1.0 - sqrtR1;
-    float v = sqrtR1 * (1.0 - r2);
-    float w = sqrtR1 * r2; // 或者 w = 1.0 - u - v;
-
-    // 3. 混合顶点位置得到采样点
-    vec3 samplePoint = vec3(worldPositions[0] * u +
-    worldPositions[1] * v +
-    worldPositions[2] * w);
-
-    return samplePoint;
-}
-
-void sampleLight(vec3 tri0WorldPositions[3], vec3 tri1WorldPositions[3], inout uint seed, out vec3 samplePos, out float pdf)
+void sampleLight(vec3 tri0WorldPositions[3], vec3 tri1WorldPositions[3], inout uint seed, out LightSampleRes sampleRes, out float pdf)
 {
     float area0 = cacTriangleArea(tri0WorldPositions);
     float area1 = cacTriangleArea(tri1WorldPositions);
@@ -100,10 +38,15 @@ void sampleLight(vec3 tri0WorldPositions[3], vec3 tri1WorldPositions[3], inout u
     float p = rnd(seed);
     float ratio = area0 / (area0 + area1);
     if(p <= ratio){
-        samplePos = uniformSampleTriangle(tri0WorldPositions, seed);
+        sampleRes.pos = uniformSampleTriangle(tri0WorldPositions, seed);
+        sampleRes.normal = getTriNormalFromTriPositions(tri0WorldPositions);
     }else{
-        samplePos = uniformSampleTriangle(tri1WorldPositions, seed);
+        sampleRes.pos = uniformSampleTriangle(tri1WorldPositions, seed);
+        sampleRes.normal = getTriNormalFromTriPositions(tri1WorldPositions);
     }
+    Material lightMat = materials.mats[lightData.lightGeometryIndex];
+    sampleRes.emit = lightMat.emission;
+    
     pdf = 1.0f / (area0 + area1);
 }
 
@@ -120,9 +63,23 @@ void unpackLightWorldPositions(out vec3 tri0WorldPositions[3], out vec3 tri1Worl
     }
 }
 
-void main()
+void sampleLight(inout uint seed, out LightSampleRes sampleRes, out float pdf)
+{
+    vec3 tri0Positions[3];
+    vec3 tri1Positions[3];
+    unpackLightWorldPositions(tri0Positions, tri1Positions);
+    sampleLight(tri0Positions, tri1Positions, seed, sampleRes, pdf);
+}
+
+uint getSeed()
 {
     uint seed = tea(gl_LaunchIDEXT.y * gl_LaunchSizeEXT.x + gl_LaunchIDEXT.x, cam.frame);
+    return seed;
+}
+
+Ray getRayFromCamera(float tmin, float tmax)
+{
+    uint seed = getSeed();
 
     float r1 = rnd(seed);
     float r2 = rnd(seed);
@@ -131,15 +88,52 @@ void main()
     const vec2 pixelCenter = vec2(gl_LaunchIDEXT.xy) + subpixel_jitter;
     const vec2 inUV = pixelCenter / vec2(gl_LaunchSizeEXT.xy);
     vec2 d = inUV * 2.0 - 1.0;
-
+    
     vec4 origin = cam.viewInverse * vec4(0, 0, 0, 1);
     vec4 target = cam.projInverse * vec4(d.x, d.y, 1, 1);
     vec4 direction = cam.viewInverse * vec4(normalize(target.xyz), 0.0);
+    
+    Ray ray;
+    ray.origin = origin.xyz;
+    ray.direction = direction.xyz;
+    ray.tmin = tmin;
+    ray.tmax = tmax;
+    return ray;
+}
 
-    float tmin = 0.001;
-    float tmax = 10000.0;
+vec3 cacDirectLight(vec3 pos, vec3 wo)
+{
+    // TODO: 计算pos处，沿着wo方向的直接光照
+    vec3 L_dir = vec3(0.0);
+    float pdf_light = 0;
+    LightSampleRes lightSampleRes;
+    return vec3(0.0);
+}
+
+vec3 pathTracing(int maxBounce)
+{
+    Ray ray = getRayFromCamera(0.001, 10000.0);
+    uint rayFlags = gl_RayFlagsOpaqueEXT;
+    vec3 totalRadiance = vec3(0.0);
+    uint seed = getSeed();
+    for(int i = 0; i < maxBounce; ++i){
+        traceRayEXT(topLevelAS, rayFlags, 0xff, 0, 0, 0, ray.origin, ray.tmin, ray.direction, ray.tmax, 0);
+        if(hitValue.dis > 0) {
+            totalRadiance += cacDirectLight(hitValue.worldPos, -ray.direction);
+            ray.origin = hitValue.worldPos;
+            ray.direction = uniformSample(-ray.direction, hitValue.worldNormal, seed);
+        }else{
+            break;
+        }
+    }
+    return totalRadiance;
+}
+
+void main()
+{
+    Ray ray = getRayFromCamera(0.001, 10000.0);
     hitValue.mat.baseColor = vec4(0.0, 0.0, 0.0, 1.0);
-    traceRayEXT(topLevelAS, gl_RayFlagsNoneEXT, 0xff, 0, 0, 0, origin.xyz, tmin, direction.xyz, tmax, 0);
+    traceRayEXT(topLevelAS, gl_RayFlagsNoneEXT, 0xff, 0, 0, 0, ray.origin, ray.tmin, ray.direction, ray.tmax, 0);
 
     // de-noising
     vec3 normal01 = getNormal01(hitValue.worldNormal);
